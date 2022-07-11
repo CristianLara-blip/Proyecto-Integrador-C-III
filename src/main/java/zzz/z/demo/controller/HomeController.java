99a4330f98995c6e860b69a06133410d28848853
Cristian Lara Arcos <cristian.lara@upeu.edu.pe>
2022-07-11 07:41:02 -0500

    VD

--- src/main/java/zzz/z/demo/controller/HomeController.java
+++ src/main/java/zzz/z/demo/controller/HomeController.java
@@ -6,10 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- *
- * @author admin
- */
 @Controller
 
 public class HomeController {
